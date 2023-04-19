41345a25f5b37e1b56d92f0e676ee268763f32fd
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 16:46:51 -0300

    E 10. No me salio

--- main.c
+++ main.c
@@ -5,3 +5,25 @@
 //caracteres alfabéticos en minúscula y C en mayúscula.
 //b. La finalización de una cadena de caracteres se representa mediante ´/0´
 
+#include<stdio.h>
+void trans(char *);
+int main(){
+    char texto[1000], *textoptr;
+    printf("Ingrese un texto:\n");
+    gets(texto);
+    printf("El texto ingresado es: ");
+    puts(texto);
+    trans(textoptr);
+    printf("El texto transformado es:");
+    puts(texto);
+    return 0;
+}
+
+void trans(char *textoptr){
+    int i;
+    for(i=0; textoptr[i]!='\0'; i++){
+        if(textoptr[i]>=97 && textoptr[i]<=122){
+            textoptr[i]=textoptr[i]-32;
+        }
+    }
+}
